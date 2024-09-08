dad39d1e0b9a102601538f6750b5556862d24e6d
engalar <engalar@gmail.com>
2024-09-08 19:04:50 +0800

    feat: first mount eval custom code
    know issue: remove filter not update datasource

--- src/WengaoReactSnippet.tsx
+++ src/WengaoReactSnippet.tsx
@@ -1,16 +1,71 @@
-import { ReactElement, createElement, useEffect } from "react";
+import { ReactElement, createElement, useEffect, createRef } from "react";
 
 import { WengaoReactSnippetContainerProps } from "../typings/WengaoReactSnippetProps";
 
 import "./ui/WengaoReactSnippet.css";
 
 export function WengaoReactSnippet({ customCode, content }: WengaoReactSnippetContainerProps): ReactElement {
+  const ref = createRef<HTMLDivElement>();
   useEffect(() => {
-    try {
-      eval(customCode);
-    } catch (error) {
-      console.error(error);
+    const hostElement = ref.current!;
+    const mutationObserver = new MutationObserver(() => {
+      try {
+        eval(customCode);
+        // hardcode(hostElement);
+      } catch (error) {
+        console.error(error);
+      }
+      mutationObserver.disconnect();
+    });
+    mutationObserver.observe(hostElement, { childList: true });
+    return () => {
+      mutationObserver.disconnect();
+    };
+  }, []);
+  return <div ref={ref}>{content}</div>;
+}
+
+// @ts-ignore
+function hardcode(hostElement) {
+  //.table-content .tr .th .column-container .filter .dropdown-container
+  const tr = hostElement.querySelector('.table-content .tr');
+  const mutaionObserver = new MutationObserver((mutations) => {
+    mutations.forEach(m => {
+      if (m.target === tr) {
+        console.log('FreeLayout[mutaionObserver] mutation', m);
+        m.addedNodes.forEach(onAdd(hostElement));
+        m.removedNodes.forEach((thElement) => {
+          const id = getIdClass(thElement);
+          if (id) {
+            const placeholer = hostElement.querySelector('.alert-info.' + id);
+            placeholer.innerHTML = '';
+          }
+        });
+      }
+    });
+  });
+  mutaionObserver.observe(tr, { childList: true, subtree: true });
+
+  // first add th
+  tr.querySelectorAll('.th').forEach(onAdd(hostElement));
+}
+
+// @ts-ignore
+function onAdd(hostElement) {
+  // @ts-ignore
+  return (thElement) => {
+    const filter = thElement.querySelector('.filter');
+    const id = getIdClass(thElement) || (filter.children.length > 0 ? getIdClass(filter.children[0]) : null);
+    if (id) {
+      thElement.classList.add(id);
+      const placeholer = hostElement.querySelector('.alert-info.' + id);
+      placeholer.appendChild(filter.children[0]);
     }
-  }, [customCode]);
-  return <div>{content}</div>;
+  };
+}
+
+// @ts-ignore
+function getIdClass(dropdown) {
+  // @ts-ignore
+  return Array.from(dropdown.classList).find(c => c.startsWith('f-'))
 }
